0ebdfde983d9b3c50f3f4071488237bbc88bde0a
Junhui Li <junhui.lee@icloud.com>
2024-09-14 13:32:08 +0800

    update: install script

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #bash
+
+if [ -e ~/.zshrc ]; then
+    mv ~/.zshrc ~/.zshrc_backup
+    cp ./zsh/.zshrc ~/.zshrc
+fi
+
+if [ -d ~/.config/nvim ]; then
+    mv ~/.config/nvim ~/.config/nvim_bak -r
+    cp -r ./nvim ~/.config/nvim -r
+fi
